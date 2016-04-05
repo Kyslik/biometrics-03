a75502cceb8f1081b2a95f7a42fde5d9c56c4cc4
Martin Kiesel <martin.kiesel@gmail.com>
2016-04-06 00:23:12 +0200

    adding shift (circshift)

--- biometrics-3/main.cpp
+++ biometrics-3/main.cpp
@@ -8,9 +8,27 @@
 
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
-int main(int argc, const char * argv[]) {
-    
-    std::cout << "CV ver. :" << CV_VERSION << std::endl;
+#include "shift.hpp"
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, const char * argv[])
+{
+    Mat image, image2, image3;
+    image = imread("./data/001_2_4_mask.bmp", IMREAD_UNCHANGED);
+    image2 = imread("./data/004_2_2_mask.bmp", IMREAD_UNCHANGED);
+
+
+
+    namedWindow("i1", WINDOW_AUTOSIZE );
+    imshow("i1", image);
+    shift(image, image, Point(150, 0), BORDER_WRAP);
+    namedWindow("i2", WINDOW_AUTOSIZE );
+    imshow("i2", image);
+    waitKey(0);
     return 0;
 }
